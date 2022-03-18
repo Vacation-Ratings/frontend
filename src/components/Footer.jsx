96daf2cbe5cf041c623bf5bb915b2ca51477f135
Keian Anthony <KeianAnthony1@gmail.com>
2022-03-18 11:34:32 -0700

    michaels changes for css styling and component reshuffle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import '../css/Footer.css';
 
 function Footer() {
+  let style = {
+    margin: "0rem 3rem 1rem",
+  }
   return (
-    <div className="footer">
+    <div className="footer" style={style}>
       <h6>
         &copy; Vacation Ratings Inc.
       </h6>
     </div>
-  )
+  );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,34 @@ import '../css/Header.css';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import { useAuth0 } from "@auth0/auth0-react";
+import VacationRatings from './images/VacationRatings-slim.jpg';
 
 function Header() {
   const { isAuthenticated } = useAuth0();
   return (
     <div>
-      <a data-testid="navLink1" className="navLink" href="/search">
-        Search
-      </a>
-      <a data-testid="navLink2" className="siteTitle" href="/">
-        Vacation Ratings
-      </a>
-      {isAuthenticated ? (
-        <a data-testid="navLink3" className="navLink" href="/addreview">
-          Add a Review
+      <div>
+        <a data-testid="navLink2" className="siteTitle" href="/">
+          <img src={VacationRatings} alt='Vacation Ratings' />
         </a>
-      ) : null}
-      {isAuthenticated ? (
-        <LogoutButton />
-      ) : (
-        <LoginButton />
-      )}
+      </div>
+      <div>
+        <a data-testid="navLink1" className="navLink" href="/search">
+          Search
+        </a>
+        {isAuthenticated ? (
+          <a data-testid="navLink3" className="navLink" href="/addreview">
+            Add a Review
+          </a>
+        ) : null}
+        {isAuthenticated ? (
+          <LogoutButton />
+        ) : (
+          <LoginButton />
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header;
